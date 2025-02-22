95f46658520e8f9bd6406f59d5def643bc6851a2
Cristian <cristian.v.chirica@gmail.com>
2025-02-22 13:27:42 +0000

    Week 1 Cristian

---
+++ Week 1/Output FileFor Week 1.py
@@ -0,0 +1,25 @@
+print("Welcome to \"QHO426\"") # This is a escape character
+print("This is a backlash\\")
+
+#Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+
+#Acrivity 1
+print("We are going to mke the code interactive")
+print("Please enter your Surname name")
+#input
+surname=input("please enter your Surname name")
+firstname=input("please enter your first name")
+lucky_number=input("please enter your lucky number")
+print("Your full name is:",surname,firstname)
+print("Your lucky number is:",lucky_number)
+#Let's add 10 to the lucky number
+
+
+print("your pasSword is:",surname+firstname+lucky_number)
+print(f"Your unique password is:{surname}{lucky_number}")
+
+#Activity 2
