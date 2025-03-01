97e543e5ef751119d20b84e1b27785940f35f89c
Cristian <cristian.v.chirica@gmail.com>
2025-03-01 12:55:16 +0000

    Week 1

---
+++ Week 1/Week 2.py
@@ -0,0 +1,5 @@
+first_name = input("please enter your first name")
+surname = input("please enter your surname name")
+lucky_number = input("please enter your lucky number")
+if lucky_number >500:
+    print("Your number is grater than 500")
